ee0a60d65aa2e9078c154a028a753f266a0ba8de
shakilhasan <shakilhasan105268@gmail.com>
2022-02-15 10:35:20 +0600

    update Asynchronous

--- Asynchronous.js
+++ Asynchronous.js
@@ -39,26 +39,26 @@
 //Asynchronous control by callback---------------
 {
     console.log("Asynchronous control by callback------------------")
-    const  takeOrder=(customer,callback)=>{
+    const takeOrder = (customer, callback) => {
         console.log(`a.take order for ${customer}`)
         callback(customer)
     };
 
-    const processOrder=(customer,callback)=>{
+    const processOrder = (customer, callback) => {
         console.log(`b.processing order for ${customer}`);
         setTimeout(() => {
             console.log(`c.cooking completed`)
             console.log(`d.order processed for ${customer}`);
             callback(customer);
-        },3000);
+        }, 3000);
     };
 
-    const completedOrder=(customer)=>{
+    const completedOrder = (customer) => {
         console.log(`e.completed order for ${customer}`);
     };
 
-    takeOrder('customer1',(customer)=>{
-        processOrder(customer,(customer)=>{
+    takeOrder('customer1', (customer) => {
+        processOrder(customer, (customer) => {
             completedOrder(customer);                       //callback hell
         })
     });
@@ -68,32 +68,108 @@
 
 
 //Asynchronous control by Promise---------------
+//ex-1
 {
     console.log("Asynchronous control by Promise------------------")
-    const  hasMeeting=false;
+    const hasMeeting = false;
 
-    const meeting= new Promise((resolve,reject)=>{
-        if(!hasMeeting){
-            const meetingDetails={
-                name:'Technical Meeting',
-                Location: 'Google Meet',
-                time:'10:00 PM'
+    const meeting = new Promise((resolve, reject) => {
+        if (!hasMeeting) {
+            const meetingDetails = {
+                name: 'Technical Meeting',
+                location: 'Google Meet',
+                time: '10:00 PM'
             };
             resolve(meetingDetails);
-        }else{
+        } else {
             reject(new Error('meeting already scheduled!'));
         }
     });
 
-    const addToCalendar=(meetingDetails)=> {
+    const addToCalendar = (meetingDetails) => {
         const calendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
         return Promise.resolve(calendar);
     }
 
-    meeting.then(addToCalendar).then((res)=>{
+    meeting.then(addToCalendar).then((res) => {
         console.log(res);
-    }).catch ((err)=>{
+    }).catch((err) => {
         console.log(err.message);
     })
 
+}
+
+//ex-2
+{
+    console.log("ex-2 -------------")
+    const promise1 = Promise.resolve(`Promise 1 resolved`);
+    const promise2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Promise 2 resolved`);
+        }, 2000);
+    });
+
+    promise1.then((res) => console.log(res));
+    promise2.then((res) => console.log(res));
+
+    Promise.all([promise1, promise2]).then((res) => {   //return result as Array
+        console.log(res);
+    });
+}
+
+//ex-3
+{
+    console.log("ex-3 -------------")
+
+    const promise1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Promise 1 resolved`);
+        }, 5000);
+    });
+    const promise2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Promise 2 resolved`);
+        }, 2000);
+    });
+
+    Promise.race([promise1, promise2]).then((res) => {  //return 1st resolved  result
+        console.log(res);
+    });
+}
+
+
+//Asynchronous control by async-await---------------
+{
+    console.log("Asynchronous control by async-await------------------")
+    const hasMeeting = false;
+
+    const meeting = new Promise((resolve, reject) => {
+        if (!hasMeeting) {
+            const meetingDetails = {
+                name: 'Technical Meeting',
+                location: 'Google Meet',
+                time: '10:00 PM'
+            };
+            resolve(meetingDetails);
+        } else {
+            reject(new Error('meeting already scheduled!'));
+        }
+    });
+
+    const addToCalendar = (meetingDetails) => {
+        const calendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
+        return Promise.resolve(calendar);
+    }
+
+    async function MyMeeting() {
+        try {
+            const meetingDetails = await meeting;
+            const calendar = await addToCalendar(meetingDetails);
+            console.log(calendar)
+        } catch {
+            console.log("Something went wrong")
+        }
+    }
+
+    MyMeeting();
 }
