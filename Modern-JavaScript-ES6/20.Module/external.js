530e58804b053600743019c87e434ea2a8f6fa4b
hasan <shakilhasan105268@gmial.com>
2022-02-28 23:36:59 +0600

    *WIP

---
+++ Modern-JavaScript-ES6/20.Module/external.js
@@ -0,0 +1,13 @@
+//named export--------
+export const pi = 3.14;
+export const a = 2.9;
+
+export function myFunc() { //function export
+    console.log('I am myFunc()');
+}
+
+//default export---------
+const b = 2.9;
+export default b;
+
+
---
+++ Modern-JavaScript-ES6/20.Module/playground.js
@@ -0,0 +1,24 @@
+//named import-----------------
+import {pi, a, myFunc} from './external.js'; //named import, no need '.js' in react & others libraries
+console.log(pi, a);
+myFunc();
+
+//alias import
+import {pi as varPi, a as varA} from './external.js'; //named import
+console.log(varPi, varA);
+
+// import all as test
+import * as test from './external.js'; //named import
+console.log(test) //get all as object
+
+
+//default import------------
+import external from './external.js'; //default import
+console.log(external)
+
+//default & named import at a time ------------
+import external1, {pi as myPi, a as myA} from './external.js'; //default import
+console.log(external1, myPi, myA)
+
+
+
