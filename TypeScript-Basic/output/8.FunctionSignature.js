7b075cc1a32a81382522cd6cd9a0cadcfd29df81
Md.Shakil Hasan Ibn Younus <shakilhasan105268@gmail.com>
2022-06-12 19:16:46 +0600

    *WIP

---
+++ TypeScript-Basic/output/8.FunctionSignature.js
@@ -0,0 +1,11 @@
+"use strict";
+{
+    // Function Signature
+    let add;
+    add = (a, b) => {
+        return a + b;
+    };
+    console.log(add(1, 3));
+}
+{
+}
---
+++ TypeScript-Basic/src/8.FunctionSignature.ts
@@ -0,0 +1,11 @@
+{
+    // Function Signature
+    let add:(x:number, b:number)=> number;
+    add=(a:number,b:number)=>{
+        return a+b;
+    }
+    console.log(add(1,3));
+}
+{
+
+}
