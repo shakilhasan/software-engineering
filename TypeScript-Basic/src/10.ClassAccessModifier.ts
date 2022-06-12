4bb6ee79f5c5e84e39c3fa9aaf03006afeb92759
Md.Shakil Hasan Ibn Younus <shakilhasan105268@gmail.com>
2022-06-12 22:12:36 +0600

    implement ts Class Access Modifier

---
+++ TypeScript-Basic/src/10.ClassAccessModifier.ts
@@ -0,0 +1,35 @@
+{
+    //Access Modifier
+    class Player{
+        private name:string;
+        public age:number;
+        readonly country:string;
+        constructor(n:string,a:number,c:string) {
+            this.name=n;
+            this.age=a;
+            this.country=c;
+        }
+        play(){
+            console.log(`${this.name} from ${this.country} is playing`);
+        }
+    }
+    const sakib = new Player('Sakib', 40, 'BD');
+    const tamim= new Player('Tamim', 45, 'BD');
+    // sakib.name='sakib-1' //error, private property can not change
+    sakib.age=50;
+    // sakib.country='BD-1'; //error, readonly property can not change
+    // console.log(sakib.name); //error, private property can not access
+    console.log(sakib.age);
+    console.log(sakib.country);
+}
+{
+    //class shortcut declaration
+    class Player{
+        constructor(
+            private name:string,
+            public age:number,
+            readonly country:string
+         ) {}
+    }
+
+}
