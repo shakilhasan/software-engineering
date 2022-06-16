189661b684b9af5cec976fe9ad111e2d23e641a4
Md.Shakil Hasan Ibn Younus <shakilhasan105268@gmail.com>
2022-06-16 23:53:26 +0600

    add typescript

---
+++ leetcode/two-sum.ts
@@ -0,0 +1,16 @@
+function twoSum(nums: number[], target: number): number[] {
+    let len:number = nums.length
+    let a:number[] = [];
+    for (let i:number = 0; i < len-1; i++) {
+        for (let j = i+1; j < len; j++) {
+            if (nums[i] + nums[j] == target) {
+                a[0] = i;
+                a[1] = j;
+                break;
+            }
+        }
+    }
+    return a;
+}
+
+twoSum([2, 7, 11, 15], 9);
