53f3f87ca5135caa0627eeb4cbef866d64ba24bf
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-08-02 00:03:01 +0600

    solve https://leetcode.com/problems/number-of-1-bits

--- #leetcode/242.Valid Anagram.ts
+++ #leetcode/242.Valid Anagram.ts
@@ -1,4 +1,18 @@
+// https://leetcode.com/problems/valid-anagram
 function isAnagram(s: string, t: string): boolean {
+    let a = [...s];
+    let b=[...t];
+
+    for(let i=0; i<a.length; i++){
+        const index = b.indexOf(a[i]);
+        if (index !== -1) {
+            b.splice(index, 1);
+        }
+    }
+    if(b.length || s.length!=t.length) return false
+    return true
+}
+function isAnagram2(s: string, t: string): boolean {
     let hash:any={}
     for(let i=0;i<s.length;i++){
         if(hash[s[i]])hash[s[i]]=hash[s[i]]+1
@@ -9,5 +23,4 @@ function isAnagram(s: string, t: string): boolean {
     const valuesArray: number[] = Object.values(hash);
     const sum: number = valuesArray.reduce((acc, curr) => acc + curr, 0);
     return !sum;
-};
-console.log(isAnagram("anagram","nagaram"))
+}
