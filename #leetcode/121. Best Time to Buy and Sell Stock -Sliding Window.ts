4b80edccc6dcd52577324304d7f5bb23e20b573c
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-08-07 11:08:37 +0600

    solved https://leetcode.com/problems/best-time-to-buy-and-sell-stock

---
+++ #leetcode/121. Best Time to Buy and Sell Stock -Sliding Window.ts
@@ -0,0 +1,10 @@
+function maxProfit(prices: number[]): number {
+    let maxDiff = 0;
+    let minVal = prices[0];
+    if (prices.length < 2) return maxDiff;
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < minVal) minVal = prices[i];
+        else if ((prices[i] - minVal) > maxDiff) maxDiff = prices[i] - minVal;
+    }
+    return maxDiff;
+}
