b04a2e0a7cb03b7a6c45cd77a19eb0e7eea94f77
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-08-08 07:12:30 +0600

    solved  https://leetcode.com/problems/merge-two-sorted-lists

---
+++ #leetcode/21.Merge Two Sorted Lists -Linked List.ts
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/merge-two-sorted-lists
+import {ListNode, LinkList} from "./helpers/Class";
+
+function mergeTwoLists(list1: ListNode | null | undefined, list2: ListNode | null | undefined): ListNode | null | undefined {
+    let result: ListNode | null | undefined = new ListNode();
+    let resultHead = result;
+    let pre: ListNode | null | undefined = new ListNode();
+    if (!list1 && !list2) return list1;
+    while ((list1 || list2) && result) {
+        if ((list1 && !list2) || (list2 && list1 && (list1?.val <= list2?.val))) {
+            result.val = list1?.val;
+            result.next = new ListNode();
+            list1 = list1.next;
+        } else if ((!list1 && list2) || (list2 && list1 && (list1.val > list2.val))) {
+            result.val = list2.val;
+            result.next = new ListNode();
+            list2 = list2.next;
+        }
+        pre = result;
+        result = result?.next;
+    }
+    pre.next = null;
+    return resultHead;
+}
--- #leetcode/helpers/Class.ts
+++ #leetcode/helpers/Class.ts
@@ -2,8 +2,32 @@
 export class ListNode {
     val: number
     next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val === undefined ? 0 : val)
-        this.next = (next === undefined ? null : next)
+    constructor( val?: number,  next?: ListNode | null) {
+        this.val = val ?? 0
+        this.next = next ?? null
     }
+}
+
+export class LinkList {
+    head: ListNode | null;
+    size: number;
+
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    push(val: number) {
+        let node=new ListNode(val);
+        if(this.head===null)this.head=node;
+        else {
+            let current: ListNode | undefined =this.head;
+            while (current){
+                if(current.next===null)current.next=node;
+                current=current.next;
+            }
+        }
+        this.size++;
+    }
+
 }
