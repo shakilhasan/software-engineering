fd054bbcdc495d05f1731c73064a2e4091853c9a
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-08-08 10:15:05 +0600

    WIP

--- #leetcode/206. Reverse Linked List.ts
+++ #leetcode/206. Reverse Linked List.ts
@@ -1,8 +1,14 @@
-//https://leetcode.com/problems/reverse-linked-list/
-// Definition for singly-linked list.
-
+//https://leetcode.com/problems/reverse-linked-list
 import {ListNode} from "./helpers/Class";
-
 function reverseList(head: ListNode | null): ListNode | null {
+    let result:ListNode | null=new ListNode();
+    let current=head
+    while (current){
+        let pre:ListNode | null=new ListNode();
+
+        result.val=current.val;
+        pre.next=result;
+        current=current.next;
+    }
     return null;
 }
