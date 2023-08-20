dcdd922099097a46aec40005f94f06fec63f17fe
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-08-21 02:22:51 +0600

    add more methods in LinkList

--- #leetcode/helpers/Class.js
+++ #leetcode/helpers/Class.js
@@ -32,24 +32,27 @@ class LinkList {
         this.length++;
     }
     pop() {
-        var _a;
-        if (this.head === null)
+        var _a, _b;
+        if (((_a = this.head) === null || _a === void 0 ? void 0 : _a.next) === null) {
+            this.head = null;
             return;
+        }
         let current = this.head;
         while (current) {
-            if (((_a = current.next) === null || _a === void 0 ? void 0 : _a.next) === null) {
+            if (((_b = current.next) === null || _b === void 0 ? void 0 : _b.next) === null) {
                 current.next = null;
                 this.length--;
                 break;
             }
             current = current.next;
         }
+        return;
     }
     toString() {
         let current = this.head;
         let result = '';
         while (current) {
-            result += current.val + '->';
+            result += current.val + '>';
             current = current.next;
         }
         return result;
@@ -95,6 +98,74 @@ class LinkList {
         }
         return i;
     }
+    insertAt(val, index) {
+        let current = this.head, i = 0;
+        if (index === 0) {
+            this.head = new LinkListNode({ val, next: this.head });
+            return;
+        }
+        while (current) {
+            if ((index - 1) === i++) {
+                current.next = new LinkListNode({ val, next: current.next });
+            }
+            current = current.next;
+        }
+    }
+    removeFrom(index) {
+        let current = this.head, i = 0, pre;
+        while (current) {
+            if ((index - 1) === i++) {
+                pre = current.next;
+            }
+            pre = current;
+            current = current.next;
+        }
+    }
+    isEmpty() {
+        return this.head === null;
+    }
+    includes(val) {
+        let current = this.head;
+        while (current) {
+            if (current.val === val)
+                return true;
+            current = current.next;
+        }
+        return false;
+    }
+    reverse() {
+        let current = this.head, pre = null;
+        while (current) {
+            pre = new LinkListNode({ val: current.val, next: pre });
+            if (current.next === null) {
+                this.head = pre;
+            }
+            current = current.next;
+        }
+    }
+    findIndex(val) {
+        let current = this.head, i = 0;
+        while (current) {
+            if (current.val === val)
+                return i;
+            current = current.next;
+            i++;
+        }
+        return -1;
+    }
+    findLastIndex(val) {
+        let current = this.head, i = 0, lastIndex = -1;
+        while (current) {
+            if (current.val === val)
+                lastIndex = i;
+            current = current.next;
+            i++;
+        }
+        return lastIndex;
+    }
+    clear() {
+        this.head = null;
+    }
 }
 exports.LinkList = LinkList;
 const list = new LinkList();
@@ -106,7 +177,14 @@ list.push("last");
 // list.pop();
 // list.join(10);
 // list.shift()
-list.unshift("first");
-list.concat(new LinkListNode({ val: 10 }));
+list.unshift(0);
+// list.concat(new LinkListNode({val: 10}));
+// list.insertAt('newInserted', 0);
+// list.removeFrom(0);
+// list.reverse();
 console.log(list.toString());
-console.log(list.length);
+// console.log(list.findLastIndex(3));
+// console.log(list.findIndex(3));
+// console.log(list.isEmpty());
+// console.log(list.includes(1));
+// console.log(list.length);
--- #leetcode/helpers/Class.ts
+++ #leetcode/helpers/Class.ts
@@ -40,7 +40,10 @@ export class LinkList {
     }
 
     pop() {
-        if (this.head === null) return;
+        if (this.head?.next === null) {
+            this.head = null;
+            return;
+        }
         let current: LinkListNode | null = this.head;
         while (current) {
             if (current.next?.next === null) {
@@ -50,13 +53,14 @@ export class LinkList {
             }
             current = current.next;
         }
+        return;
     }
 
     toString() {
         let current: LinkListNode | null = this.head;
         let result = '';
         while (current) {
-            result += current.val + '->';
+            result += current.val + '>';
             current = current.next;
         }
         return result;
@@ -67,7 +71,7 @@ export class LinkList {
         this.length += this.length - 1;
         while (current) {
             if (current.next) {
-                current.next = new LinkListNode({val, next:current.next})
+                current.next = new LinkListNode({val, next: current.next})
                 current = current.next.next;
             } else break;
         }
@@ -80,7 +84,7 @@ export class LinkList {
 
     unshift(val: any) {
         this.length += 1;
-        this.head = new LinkListNode({val, next:this.head});
+        this.head = new LinkListNode({val, next: this.head});
     }
 
     concat(node: LinkListNode | null) {
@@ -104,7 +108,77 @@ export class LinkList {
         return i;
     }
 
+    insertAt(val: any, index: number) {
+        let current = this.head, i = 0;
+        if (index === 0) {
+            this.head = new LinkListNode({val, next: this.head})
+            return;
+        }
+        while (current) {
+            if ((index - 1) === i++) {
+                current.next = new LinkListNode({val, next: current.next})
+            }
+            current = current.next;
+        }
+    }
 
+    removeFrom(index: number) {
+        let current = this.head, i = 0, pre;
+        while (current) {
+            if ((index - 1) === i++) {
+                pre = current.next;
+            }
+            pre = current;
+            current = current.next;
+        }
+    }
+
+    isEmpty() {
+        return this.head === null;
+    }
+
+    includes(val: any) {
+        let current = this.head;
+        while (current) {
+            if (current.val === val) return true
+            current = current.next;
+        }
+        return false;
+    }
+
+    reverse() {
+        let current = this.head, pre = null;
+        while (current) {
+            pre = new LinkListNode({val: current.val, next: pre})
+            if (current.next === null) {
+                this.head = pre;
+            }
+            current = current.next;
+        }
+    }
+
+    findIndex(val: any) {
+        let current = this.head, i = 0;
+        while (current) {
+            if (current.val === val) return i;
+            current = current.next;
+            i++;
+        }
+        return -1;
+    }
+
+    findLastIndex(val: any) {
+        let current = this.head, i = 0, lastIndex = -1;
+        while (current) {
+            if (current.val === val) lastIndex = i;
+            current = current.next;
+            i++;
+        }
+        return lastIndex;
+    }
+    clear(){
+        this.head=null;
+    }
 }
 
 const list = new LinkList();
@@ -116,8 +190,15 @@ list.push("last");
 // list.pop();
 // list.join(10);
 // list.shift()
-list.unshift("first")
-list.concat(new LinkListNode({val:10}));
+list.unshift(0);
+// list.concat(new LinkListNode({val: 10}));
+// list.insertAt('newInserted', 0);
+// list.removeFrom(0);
+// list.reverse();
 console.log(list.toString());
-console.log(list.length);
+// console.log(list.findLastIndex(3));
+// console.log(list.findIndex(3));
+// console.log(list.isEmpty());
+// console.log(list.includes(1));
+// console.log(list.length);
 
