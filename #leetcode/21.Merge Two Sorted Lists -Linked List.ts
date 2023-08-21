173d248fe0c72fe9c87cb7761bdfb4d3cfa6873c
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-08-22 01:37:41 +0600

    add mergeTwoLists1

--- #leetcode/21.Merge Two Sorted Lists -Linked List.ts
+++ #leetcode/21.Merge Two Sorted Lists -Linked List.ts
@@ -11,14 +11,30 @@ function mergeTwoLists(list1: LinkListNode | null | undefined, list2: LinkListNo
             result.val = list1.val;
             result.next = new LinkListNode({});
             list1 = list1.next;
-        } else if ((!list1 && list2) || (list2 && list1 && (list1.val > list2.val))) {
-            result.val = list2.val;
+        } else {
+            result.val = list2?.val;
             result.next = new LinkListNode({});
-            list2 = list2.next;
+            list2 = list2?.next;
         }
         pre = result;
         result = result?.next;
     }
     pre.next = null;
     return resultHead;
+}
+function mergeTwoLists1(list1: LinkListNode | null , list2: LinkListNode | null ): LinkListNode | null  {
+    let resultHead: LinkListNode | null = new LinkListNode({});
+    let current: LinkListNode | null = resultHead;
+    while ((list1 || list2) && current) {
+        if ((list1 && !list2) || (list2 && list1 && (list1?.val <= list2?.val))) {
+            current.next = list1
+            list1=list1.next;
+        }
+        else {
+            current.next = list2;
+            if(list2) list2=list2?.next;
+        }
+        current = current.next
+    }
+    return resultHead.next;
 }
