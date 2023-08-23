41ad9baea380f4b0212a63b132b565f040c1781d
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-08-23 10:58:35 +0600

    update hasCycle1()

--- #leetcode/141. Linked List Cycle.js
+++ #leetcode/141. Linked List Cycle.js
@@ -10,3 +10,13 @@ function hasCycle(head) {
     }
     return false;
 }
+function hasCycle1(head) {
+    let fast = head, slow = head;
+    while (fast === null || fast === void 0 ? void 0 : fast.next) {
+        fast = fast.next.next;
+        slow = slow === null || slow === void 0 ? void 0 : slow.next;
+        if (fast === slow)
+            return true;
+    }
+    return false;
+}
--- #leetcode/21.Merge Two Sorted Lists -Linked List.js
+++ #leetcode/21.Merge Two Sorted Lists -Linked List.js
@@ -1,27 +1,44 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 //https://leetcode.com/problems/merge-two-sorted-lists
-const Class_1 = require("./helpers/LinkList");
+const LinkList_1 = require("./helpers/LinkList");
 function mergeTwoLists(list1, list2) {
-    let result = new Class_1.LinkListNode({});
+    let result = new LinkList_1.LinkListNode({});
     let resultHead = result;
-    let pre = new Class_1.LinkListNode({});
+    let pre = new LinkList_1.LinkListNode({});
     if (!list1 && !list2)
         return list1;
     while ((list1 || list2) && result) {
         if ((list1 && !list2) || (list2 && list1 && ((list1 === null || list1 === void 0 ? void 0 : list1.val) <= (list2 === null || list2 === void 0 ? void 0 : list2.val)))) {
             result.val = list1.val;
-            result.next = new Class_1.LinkListNode({});
+            result.next = new LinkList_1.LinkListNode({});
             list1 = list1.next;
         }
-        else if ((!list1 && list2) || (list2 && list1 && (list1.val > list2.val))) {
-            result.val = list2.val;
-            result.next = new Class_1.LinkListNode({});
-            list2 = list2.next;
+        else {
+            result.val = list2 === null || list2 === void 0 ? void 0 : list2.val;
+            result.next = new LinkList_1.LinkListNode({});
+            list2 = list2 === null || list2 === void 0 ? void 0 : list2.next;
         }
         pre = result;
         result = result === null || result === void 0 ? void 0 : result.next;
     }
     pre.next = null;
     return resultHead;
+}
+function mergeTwoLists1(list1, list2) {
+    let resultHead = new LinkList_1.LinkListNode({});
+    let current = resultHead;
+    while ((list1 || list2) && current) {
+        if ((list1 && !list2) || (list2 && list1 && ((list1 === null || list1 === void 0 ? void 0 : list1.val) <= (list2 === null || list2 === void 0 ? void 0 : list2.val)))) {
+            current.next = list1;
+            list1 = list1.next;
+        }
+        else {
+            current.next = list2;
+            if (list2)
+                list2 = list2 === null || list2 === void 0 ? void 0 : list2.next;
+        }
+        current = current.next;
+    }
+    return resultHead.next;
 }
