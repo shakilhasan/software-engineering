7d4659edc68b66a8ceae5f5be6dae58bb2bf6a58
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-08-24 16:18:10 +0600

    add Adapter example

--- design-patterns/2.Structural/1.Adapter/example_1.js
+++ design-patterns/2.Structural/1.Adapter/example_1.js
@@ -1,1 +1,36 @@
 "use strict";
+class Target {
+    request() {
+        return 'Target: The default target\'s behavior.';
+    }
+}
+class Adaptee {
+    specificRequest() {
+        return '.eetpadA eht fo roivaheb laicepS';
+    }
+}
+class Adapter extends Target {
+    adaptee;
+    constructor(adaptee) {
+        super();
+        this.adaptee = adaptee;
+    }
+    request() {
+        const result = this.adaptee.specificRequest().split('').reverse().join('');
+        return `Adapter: (TRANSLATED) ${result}`;
+    }
+}
+function adapterClientCode(target) {
+    console.log(target.request());
+}
+console.log('Client: I can work just fine with the Target objects:');
+const target = new Target();
+adapterClientCode(target);
+console.log('');
+const adaptee = new Adaptee();
+console.log('Client: The Adaptee class has a weird interface. See, I don\'t understand it:');
+console.log(`Adaptee: ${adaptee.specificRequest()}`);
+console.log('');
+console.log('Client: But I can work with it via the Adapter:');
+const adapter = new Adapter(adaptee);
+adapterClientCode(adapter);
--- design-patterns/2.Structural/1.Adapter/readme.md
+++ design-patterns/2.Structural/1.Adapter/readme.md
@@ -1,1 +1,2 @@
-#### For details [here](https://refactoring.guru/design-patterns)
+#### For details [here](https://refactoring.guru/design-patterns/adapter)
+Adapter is a structural design pattern that allows objects with incompatible interfaces to collaborate.
