1c9e30595ec49687a849fc9265eaa331188eac5f
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-08-31 11:14:10 +0600

    fix issue

--- design-patterns/2.Structural/7.Proxy/example_1.ts
+++ design-patterns/2.Structural/7.Proxy/example_1.ts
@@ -2,16 +2,20 @@
 interface Subject {
     request(): void;
 }
+
 class RealSubject implements Subject {
     public request(): void {
         console.log('RealSubject: Handling request.');
     }
 }
-class Proxy implements Subject {
+
+class Proxy1 implements Subject {
     private realSubject: RealSubject;
+
     constructor(realSubject: RealSubject) {
         this.realSubject = realSubject;
     }
+
     public request(): void {
         if (this.checkAccess()) {
             this.realSubject.request();
@@ -20,24 +24,24 @@ class Proxy implements Subject {
     }
 
     private checkAccess(): boolean {
-        console.log('Proxy: Checking access prior to firing a real request.');
+        console.log('Proxy1: Checking access prior to firing a real request.');
         return true;
     }
 
     private logAccess(): void {
-        console.log('Proxy: Logging the time of request.');
+        console.log('Proxy1: Logging the time of request.');
     }
 }
 
-function clientCode(subject: Subject) {
+function proxyClientCode(subject: Subject) {
     subject.request();
 }
 
 console.log('Client: Executing the client code with a real subject:');
 const realSubject = new RealSubject();
-clientCode(realSubject);
+proxyClientCode(realSubject);
 console.log('');
 
 console.log('Client: Executing the same client code with a proxy:');
-const proxy = new Proxy(realSubject);
-clientCode(proxy);
+const proxy = new Proxy1(realSubject);
+proxyClientCode(proxy);
