00e83b701c0eb87e66acae9d708ce970671e1ff7
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-08-31 16:52:58 +0600

    add Command example

--- design-patterns/3.Behavioral/2.Command/example_1.js
+++ design-patterns/3.Behavioral/2.Command/example_1.js
@@ -1,1 +1,62 @@
 "use strict";
+class SimpleCommand {
+    payload;
+    constructor(payload) {
+        this.payload = payload;
+    }
+    execute() {
+        console.log(`SimpleCommand: See, I can do simple things like printing (${this.payload})`);
+    }
+}
+class ComplexCommand {
+    receiver;
+    a;
+    b;
+    constructor(receiver, a, b) {
+        this.receiver = receiver;
+        this.a = a;
+        this.b = b;
+    }
+    execute() {
+        console.log('ComplexCommand: Complex stuff should be done by a receiver object.');
+        this.receiver.doSomething(this.a);
+        this.receiver.doSomethingElse(this.b);
+    }
+}
+class Receiver {
+    doSomething(a) {
+        console.log(`Receiver: Working on (${a}.)`);
+    }
+    doSomethingElse(b) {
+        console.log(`Receiver: Also working on (${b}.)`);
+    }
+}
+class Invoker {
+    onStart;
+    onFinish;
+    setOnStart(command) {
+        this.onStart = command;
+    }
+    setOnFinish(command) {
+        this.onFinish = command;
+    }
+    doSomethingImportant() {
+        console.log('Invoker: Does anybody want something done before I begin?');
+        if (this.isCommand(this.onStart)) {
+            this.onStart.execute();
+        }
+        console.log('Invoker: ...doing something really important...');
+        console.log('Invoker: Does anybody want something done after I finish?');
+        if (this.isCommand(this.onFinish)) {
+            this.onFinish.execute();
+        }
+    }
+    isCommand(object) {
+        return object.execute !== undefined;
+    }
+}
+const invoker = new Invoker();
+invoker.setOnStart(new SimpleCommand('Say Hi!'));
+const receiver = new Receiver();
+invoker.setOnFinish(new ComplexCommand(receiver, 'Send email', 'Save report'));
+invoker.doSomethingImportant();
--- design-patterns/3.Behavioral/2.Command/readme.md
+++ design-patterns/3.Behavioral/2.Command/readme.md
@@ -1,1 +1,4 @@
-#### For details [here](https://refactoring.guru/design-patterns)
+# Command
+Command is a behavioral design pattern that turns a request into a stand-alone object that contains all information about the request. This transformation lets you pass requests as a method arguments, delay or queue a request’s execution, and support undoable operations.
+#### For details [here](https://refactoring.guru/design-patterns/command)
+![](https://refactoring.guru/images/patterns/content/command/command-en.png)
