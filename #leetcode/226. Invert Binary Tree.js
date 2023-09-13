5c891c819acf55f8d20c6ab65d510f03f2206618
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-09-13 09:46:29 +0600

    SOLVED: https://leetcode.com/problems/invert-binary-tree

--- #leetcode/226. Invert Binary Tree.js
+++ #leetcode/226. Invert Binary Tree.js
@@ -4,13 +4,12 @@ const BinarySearchTree_1 = require("./helpers/BinarySearchTree");
 function invertTree(root) {
     if (!root)
         return root;
-    root.right = invertTree(root === null || root === void 0 ? void 0 : root.left);
-    root.left = invertTree(root === null || root === void 0 ? void 0 : root.right);
+    let temp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(temp);
     return root;
 }
-const bst = new BinarySearchTree_1.BinarySearchTree({ root: null });
+// test
+const bst = new BinarySearchTree_1.BinarySearchTree({});
 bst.insertArray([4, 2, 7, 1, 3, 6, 9]);
-// bst.inorder()
-const root = invertTree(bst.root);
-console.log(root);
-// console.log(bst.root);
+console.log(invertTree(bst.root));
--- #leetcode/226. Invert Binary Tree.ts
+++ #leetcode/226. Invert Binary Tree.ts
@@ -2,14 +2,13 @@ import {BinarySearchTree, BinarySearchTreeNode} from "./helpers/BinarySearchTree
 
 function invertTree(root: BinarySearchTreeNode | null | undefined): BinarySearchTreeNode | null | undefined {
     if (!root) return root;
-    root.right = invertTree(root?.left);
-    root.left = invertTree(root?.right);
+    let temp = root.left;
+    root.left = invertTree( root.right);
+    root.right = invertTree(temp);
     return root;
 }
 
-const bst = new BinarySearchTree({root: null})
+// test
+const bst = new BinarySearchTree({})
 bst.insertArray([4, 2, 7, 1, 3, 6, 9])
-// bst.inorder()
-const root = invertTree(bst.root);
-console.log(root);
-// console.log(bst.root);
+console.log(invertTree(bst.root));
