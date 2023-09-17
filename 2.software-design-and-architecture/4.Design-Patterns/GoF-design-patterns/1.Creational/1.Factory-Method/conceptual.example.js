0f791515f87ec9032595269cac81d66f46d5545b
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-09-17 15:25:02 +0600

    rename example_1 to conceptual.example

--- 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/1.Creational/1.Factory-Method/conceptual.example.js
+++ 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/1.Creational/1.Factory-Method/conceptual.example.js
@@ -1,43 +1,37 @@
-abstract class Creator {
-    public abstract factoryMethod(): Product;
-
-    public someOperation(): string {
+"use strict";
+// https://refactoring.guru/design-patterns/factory-method/typescript/example#example-0--index-ts
+class Creator {
+    someOperation() {
         const product = this.factoryMethod();
         return `Creator: The same creator's code has just worked with ${product.operation()}`;
     }
 }
-interface Product {
-    operation(): string;
-}
-class Product1 implements Product {
-    public operation(): string {
+class Product1 {
+    operation() {
         return '{Result of the ConcreteProduct1}';
     }
 }
 class Creator1 extends Creator {
-    public factoryMethod(): Product {
+    factoryMethod() {
         return new Product1();
     }
 }
-class Product2 implements Product {
-    public operation(): string {
+class Product2 {
+    operation() {
         return '{Result of the ConcreteProduct2}';
     }
 }
 class Creator2 extends Creator {
-    public factoryMethod(): Product {
+    factoryMethod() {
         return new Product2();
     }
 }
-
-function builderClientCode(creator: Creator) {
+function builderClientCode(creator) {
     console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
     console.log(creator.someOperation());
 }
-
 console.log('App: Launched with the ConcreteCreator1.');
 builderClientCode(new Creator1());
 console.log('');
-
 console.log('App: Launched with the ConcreteCreator2.');
 builderClientCode(new Creator2());
--- 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/1.Creational/1.Factory-Method/conceptual.example.ts
+++ 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/1.Creational/1.Factory-Method/conceptual.example.ts
@@ -1,36 +1,44 @@
-"use strict";
-class Creator {
-    someOperation() {
+// https://refactoring.guru/design-patterns/factory-method/typescript/example#example-0--index-ts
+abstract class Creator {
+    public abstract factoryMethod(): Product;
+
+    public someOperation(): string {
         const product = this.factoryMethod();
         return `Creator: The same creator's code has just worked with ${product.operation()}`;
     }
 }
-class Product1 {
-    operation() {
+interface Product {
+    operation(): string;
+}
+class Product1 implements Product {
+    public operation(): string {
         return '{Result of the ConcreteProduct1}';
     }
 }
 class Creator1 extends Creator {
-    factoryMethod() {
+    public factoryMethod(): Product {
         return new Product1();
     }
 }
-class Product2 {
-    operation() {
+class Product2 implements Product {
+    public operation(): string {
         return '{Result of the ConcreteProduct2}';
     }
 }
 class Creator2 extends Creator {
-    factoryMethod() {
+    public factoryMethod(): Product {
         return new Product2();
     }
 }
-function builderClientCode(creator) {
+
+function builderClientCode(creator: Creator) {
     console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
     console.log(creator.someOperation());
 }
+
 console.log('App: Launched with the ConcreteCreator1.');
 builderClientCode(new Creator1());
 console.log('');
+
 console.log('App: Launched with the ConcreteCreator2.');
 builderClientCode(new Creator2());
