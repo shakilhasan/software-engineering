fb45b5edc62c85d658344cbb74bcda49bc21cf55
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-09-19 18:54:58 +0600

    *WIP: add Observer example

--- 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/3.Behavioral/6.Observer/conceptual.example.js
+++ 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/3.Behavioral/6.Observer/conceptual.example.js
@@ -1,1 +1,53 @@
 "use strict";
+class ConcreteSubject {
+    state;
+    observers = [];
+    attach(observer) {
+        const isExist = this.observers.includes(observer);
+        if (isExist)
+            return console.log('Subject: Observer has been attached already.');
+        console.log('Subject: Attached an observer.');
+        this.observers.push(observer);
+    }
+    detach(observer) {
+        const observerIndex = this.observers.indexOf(observer);
+        if (observerIndex === -1)
+            return console.log('Subject: Nonexistent observer.');
+        this.observers.splice(observerIndex, 1);
+        console.log('Subject: Detached an observer.');
+    }
+    notify() {
+        console.log('Subject: Notifying observers...');
+        for (const observer of this.observers) {
+            observer.update(this);
+        }
+    }
+    someBusinessLogic() {
+        console.log('\nSubject: I\'m doing something important.');
+        this.state = Math.floor(Math.random() * (10 + 1));
+        console.log(`Subject: My state has just changed to: ${this.state}`);
+        this.notify();
+    }
+}
+class ConcreteObserverA {
+    update(subject) {
+        if (subject instanceof ConcreteSubject && subject.state < 3)
+            console.log('ConcreteObserverA: Reacted to the event.');
+    }
+}
+class ConcreteObserverB {
+    update(subject) {
+        if (subject instanceof ConcreteSubject && (subject.state === 0 || subject.state >= 2))
+            console.log('ConcreteObserverB: Reacted to the event.');
+    }
+}
+//The client code.
+const subject = new ConcreteSubject();
+const observer1 = new ConcreteObserverA();
+subject.attach(observer1);
+const observer2 = new ConcreteObserverB();
+subject.attach(observer2);
+subject.someBusinessLogic();
+subject.someBusinessLogic();
+subject.detach(observer2);
+subject.someBusinessLogic();
--- 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/3.Behavioral/6.Observer/readme.md
+++ 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/3.Behavioral/6.Observer/readme.md
@@ -1,1 +1,4 @@
+# Observer
+Observer is a behavioral design pattern that lets you define a subscription mechanism to notify multiple objects about any events that happen to the object they’re observing.
 #### For details [here](https://refactoring.guru/design-patterns)
+![](https://refactoring.guru/images/patterns/content/observer/observer.png)
