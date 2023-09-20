36ae76e50268bc330fb583d107cc6467d8090eb3
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-09-20 12:43:55 +0600

    *WIP: add Strategy example

--- 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/3.Behavioral/8.Strategy/conceptual.example.js
+++ 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/3.Behavioral/8.Strategy/conceptual.example.js
@@ -1,1 +1,33 @@
 "use strict";
+//https://refactoring.guru/design-patterns/strategy/typescript/example#example-0--index-ts
+class Context {
+    strategy;
+    constructor(strategy) {
+        this.strategy = strategy;
+    }
+    setStrategy(strategy) {
+        this.strategy = strategy;
+    }
+    doSomeBusinessLogic() {
+        console.log('Context: Sorting data using the strategy (not sure how it\'ll do it)');
+        const result = this.strategy.doAlgorithm(['a', 'b', 'c', 'd', 'e']);
+        console.log(result.join(','));
+    }
+}
+class ConcreteStrategyA {
+    doAlgorithm(data) {
+        return data.sort();
+    }
+}
+class ConcreteStrategyB {
+    doAlgorithm(data) {
+        return data.reverse();
+    }
+}
+const context = new Context(new ConcreteStrategyA());
+console.log('Client: Strategy is set to normal sorting.');
+context.doSomeBusinessLogic();
+console.log('');
+console.log('Client: Strategy is set to reverse sorting.');
+context.setStrategy(new ConcreteStrategyB());
+context.doSomeBusinessLogic();
--- 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/3.Behavioral/8.Strategy/readme.md
+++ 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/3.Behavioral/8.Strategy/readme.md
@@ -1,1 +1,4 @@
+# Strategy
+Strategy is a behavioral design pattern that lets you define a family of algorithms, put each of them into a separate class, and make their objects interchangeable.
 #### For details [here](https://refactoring.guru/design-patterns)
+![Strategy](https://refactoring.guru/images/patterns/content/strategy/strategy.png)
