955bfcdf139035feb8207a58f96cd0e1c77606e5
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-09-20 06:52:57 +0600

    *WIP: fix State pattern example

--- 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/3.Behavioral/7.State/conceptual.example.js
+++ 2.software-design-and-architecture/4.Design-Patterns/GoF-design-patterns/3.Behavioral/7.State/conceptual.example.js
@@ -1,2 +1,53 @@
 "use strict";
 //https://refactoring.guru/design-patterns/state/typescript/example#example-0--index-ts
+class Context {
+    state;
+    constructor(state) {
+        this.transitionTo(state);
+    }
+    transitionTo(state) {
+        console.log(`Context: Transition to ${state.constructor.name}.`);
+        this.state = state;
+        this.state.setContext(this);
+    }
+    request1() {
+        if (this.state)
+            this.state.handle1();
+    }
+    request2() {
+        if (this.state)
+            this.state.handle2();
+    }
+}
+class State {
+    context;
+    setContext(context) {
+        this.context = context;
+    }
+}
+class ConcreteStateA extends State {
+    handle1() {
+        console.log('ConcreteStateA handles request1.');
+        console.log('ConcreteStateA wants to change the state of the context.');
+        if (this.context)
+            this.context.transitionTo(new ConcreteStateB());
+    }
+    handle2() {
+        console.log('ConcreteStateA handles request2.');
+    }
+}
+class ConcreteStateB extends State {
+    handle1() {
+        console.log('ConcreteStateB handles request1.');
+    }
+    handle2() {
+        console.log('ConcreteStateB handles request2.');
+        console.log('ConcreteStateB wants to change the state of the context.');
+        if (this.context)
+            this.context.transitionTo(new ConcreteStateA());
+    }
+}
+//The client code.
+const context = new Context(new ConcreteStateA());
+context.request1();
+context.request2();
