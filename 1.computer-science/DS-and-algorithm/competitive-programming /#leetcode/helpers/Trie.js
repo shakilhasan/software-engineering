c66dde1b4a33b6d4f7827f643c59aca063d58238
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-10-18 17:34:42 +0600

    add Trie remove

--- 1.computer-science/DS-and-algorithm/competitive-programming /#leetcode/helpers/Trie.js
+++ 1.computer-science/DS-and-algorithm/competitive-programming /#leetcode/helpers/Trie.js
@@ -39,8 +39,35 @@ class Trie {
         }
         return current.isWord;
     }
+    isEmpty(node = this.root) {
+        for (let i = 0; i < ALPHABET_SIZE; i++) {
+            if (node.next[i])
+                return false;
+        }
+        return true;
+    }
+    remove(word) {
+        this.remove_recursion(this.root, word, 0);
+    }
+    remove_recursion(current, word, depth) {
+        if (!current)
+            return null;
+        if (word.length === depth) {
+            current.isWord = false;
+            if (this.isEmpty(current))
+                current = null;
+            return current;
+        }
+        let index = word[depth].charCodeAt(0) - "a".charCodeAt(0);
+        current.next[index] = this.remove_recursion(current.next[index], word, depth + 1);
+        if (this.isEmpty(current) && !current.isWord)
+            current = null;
+        return current;
+    }
 }
 const trie = new Trie();
 trie.insertArray(["shakil", "hasan", "nabil", "ahmad", "book", "booked",]);
 console.log(trie.root);
 console.log(trie.search("nabil"));
+trie.remove("book");
+console.log(trie.search("book"));
--- 1.computer-science/DS-and-algorithm/competitive-programming /#leetcode/helpers/Trie.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming /#leetcode/helpers/Trie.ts
@@ -35,9 +35,35 @@ class Trie {
         }
         return current.isWord;
     }
+
+    isEmpty(node: TrieNode = this.root) {
+        for (let i = 0; i < ALPHABET_SIZE; i++) {
+            if (node.next[i]) return false
+        }
+        return true;
+    }
+
+    remove(word: string) {
+        this.remove_recursion(this.root, word, 0);
+    }
+
+    remove_recursion(current: TrieNode | null, word: string, depth: number) {
+        if (!current) return null;
+        if (word.length === depth) {
+            current.isWord = false;
+            if (this.isEmpty(current)) current = null
+            return current;
+        }
+        let index = word[depth].charCodeAt(0) - "a".charCodeAt(0);
+        current.next[index] = this.remove_recursion(current.next[index], word, depth + 1);
+        if (this.isEmpty(current) && !current.isWord) current = null
+        return current;
+    }
 }
 
 const trie = new Trie();
 trie.insertArray(["shakil", "hasan", "nabil", "ahmad", "book", "booked",])
 console.log(trie.root);
 console.log(trie.search("nabil"));
+trie.remove("book")
+console.log(trie.search("book"));
