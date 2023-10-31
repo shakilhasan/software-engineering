a35d39b4d908ac1d9eb88f60c78ac8f7c3a4684b
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-10-31 18:18:42 +0600

    remove build .js file from leetcode

--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/9.PalindromeNumber.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// https://leetcode.com/problems/palindrome-number
-function isPalindromeByStr(x) {
-    let str = x.toString();
-    return str === [...str].reverse().join("");
-}
-console.log(isPalindromeByStr(12122121));
