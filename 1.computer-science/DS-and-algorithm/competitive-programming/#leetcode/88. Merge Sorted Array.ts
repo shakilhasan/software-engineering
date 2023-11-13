d36aeaeae86d06a38b5768549d483012872c1776
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-11-14 00:13:23 +0600

    [SOLVED-1]https://leetcode.com/problems/merge-sorted-array/description

--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/88. Merge Sorted Array.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/88. Merge Sorted Array.ts
@@ -1,19 +1,10 @@
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let i = 0, j = 0, p = 0, c: number[] = [];
-    while (i < m || j < n) {
-        if ((nums1[i] <= nums2[j] || j >= n) && i < m) {
-            c[p++] = nums1[i];
-            i++;
-        } else {
-            c[p++] = nums2[j];
-            j++;
-        }
+    let i = m, j = n, p = m + n;
+    while ((i || j) && p) {
+        if ((nums1[i - 1] >= nums2[j - 1] && i) || !j) nums1[--p] = nums1[--i];
+        else nums1[--p] = nums2[--j];
     }
-    // nums1=c
-    for (i = 0; i < c.length; i++) {
-        nums1[i] = c[i]
-    }
-    console.log(nums1);
 }
 
-merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+merge([4, 5, 6, 0, 0, 0], 3, [1, 2, 3], 3)
+// merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
