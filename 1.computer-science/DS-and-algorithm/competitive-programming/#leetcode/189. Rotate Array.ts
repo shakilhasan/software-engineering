959af3c419d2a9c1d72a9a62a4f20b3eefce85d0
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-11-15 07:00:44 +0600

    [SOLVED]https://leetcode.com/problems/rotate-array/description

---
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/189. Rotate Array.ts
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/rotate-array/description
+
+function rotate(nums: number[], k: number): void {
+    const reverseArray = (i: number, j: number): any => {
+        while (i < j + 1) {
+            let temp = nums[i];
+            nums[i++] = nums[j]
+            nums[j--] = temp;
+        }
+    };
+    k = k % nums.length;
+    reverseArray(0, nums.length - 1);
+    reverseArray(0, k - 1);
+    reverseArray(k, nums.length - 1);
+    // console.log(nums);
+}
+
+// rotate([1, 2, 3, 4, 5, 6, 7], 3);
+rotate([-1], 2);
