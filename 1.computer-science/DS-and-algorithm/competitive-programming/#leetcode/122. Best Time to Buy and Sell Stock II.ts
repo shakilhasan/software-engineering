25aa281f702ae984d7a2e96aa9c46df9ef5bb623
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-11-15 21:39:52 +0600

    [Accepted] https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description

--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/122. Best Time to Buy and Sell Stock II.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/122. Best Time to Buy and Sell Stock II.ts
@@ -1,11 +1,11 @@
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description
 function maxProfit1(prices: number[]): number {
-    let maxDiff = 0;
-    let minVal = prices[0];
-    if (prices.length < 2) return maxDiff;
-    for (let i = 1; i < prices.length; i++) {
-        if (prices[i] < minVal) minVal = prices[i];
-        else if ((prices[i] - minVal) > maxDiff) maxDiff = prices[i] - minVal;
+    let totalProfit=0;
+    if (prices.length < 2) return 0;
+    for (let i = 0; i < prices.length; i++) {
+        if(prices[i]<prices[i+1])totalProfit += (prices[i+1]-prices[i])
     }
-    return maxDiff;
+    return totalProfit;
 }
+
+console.log(maxProfit1([1,2,3,4,5]))
