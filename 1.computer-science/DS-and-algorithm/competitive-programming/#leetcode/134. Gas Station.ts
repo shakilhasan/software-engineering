5ad11f5eac1c5b6c682b95245521340b1eb48361
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-11-21 02:50:58 +0600

    [Accepted] https://leetcode.com/problems/gas-station/description

--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/134. Gas Station.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/134. Gas Station.ts
@@ -1,19 +1,18 @@
 //https://leetcode.com/problems/gas-station/description
 function canCompleteCircuit(gas: number[], cost: number[]): number {
-    let i = 0, circuit = 0, rest = 0, start = 0, len = gas.length, count = 0;
-    if (len === 1 && cost[0] <= gas[0]) return 0;
-    while (!(circuit > 0 && i > len - 2) && len > 1) {
+    let [i, circuit, rest, start, count] = [0, 0, 0, 0, 0];
+    while (circuit < 2) {
         if (rest + gas[i] - cost[i] >= 0) rest = rest + gas[i] - cost[i];
         else {
             start = i + 1;
             count = 0;
             rest = 0;
         }
-        if (i === len - 1) {
+        if (i === gas.length - 1) {
             i = -1;
             circuit++;
         }
-        if (count === len) return start;
+        if (count === gas.length) return start;
         i++;
         count++;
     }
@@ -23,3 +22,4 @@ function canCompleteCircuit(gas: number[], cost: number[]): number {
 console.log(canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]))
 console.log(canCompleteCircuit([2, 3, 4], [3, 4, 3]))
 console.log(canCompleteCircuit([5, 1, 2, 3, 4], [4, 4, 1, 5, 1]))
+console.log(canCompleteCircuit([5], [4]))
