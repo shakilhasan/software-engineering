b2df8208914e5d1f204f88148fb692b6f3e41cc1
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-11-27 12:23:59 +0600

    [Accepted] https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string

---
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/28. Find the Index of the First Occurrence in a String.ts
@@ -0,0 +1,6 @@
+function strStr(haystack: string, needle: string): number {
+    return haystack.indexOf(needle);
+}
+
+console.log(strStr('sadbutsad', 'sad'));
+console.log(strStr('leetcode', 'leeto'));
