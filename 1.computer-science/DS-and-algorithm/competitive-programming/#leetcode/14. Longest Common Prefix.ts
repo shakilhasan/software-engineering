c38c06fd7f4ba489181bb23706cba14111a34dc4
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-11-27 23:44:48 +0600

    [Accepted] https://leetcode.com/problems/longest-common-prefix

--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/14. Longest Common Prefix.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/14. Longest Common Prefix.ts
@@ -1,13 +1,9 @@
 // https://leetcode.com/problems/longest-common-prefix/
 function longestCommonPrefix(strs: string[]): string {
-        let size:number = strs.length;
-        if (size == 1) return strs[0];
-        strs.sort();
-        let i:number = 0;
-        while (i < strs[0].length && strs[0][i] == strs[size-1][i] )
-            i++;
-        let pre:string = strs[0].substring(0, i);
-        return pre;
-    }
+    let size: number = strs.length, i = 0;
+    strs.sort();
+    while (i < strs[0].length && strs[0][i] == strs[size - 1][i]) i++;
+    return strs[0].substring(0, i);
+}
 
-console.log( longestCommonPrefix(["geeksforgeeks", "geeks", "geek", "geezer"]));
+console.log(longestCommonPrefix(["geeksforgeeks", "geeks", "geek", "geezer"]));
