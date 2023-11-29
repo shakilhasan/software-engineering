b044c4f6a3a4cf1d623ccb332d828ec2d56351a8
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-11-29 22:33:07 +0600

    [Accepted] https://leetcode.com/problems/longest-substring-without-repeating-characters

--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/3. Longest Substring Without Repeating Characters.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/3. Longest Substring Without Repeating Characters.ts
@@ -1,22 +1,12 @@
 //https://leetcode.com/problems/longest-substring-without-repeating-characters
 function lengthOfLongestSubstring(s: string): number {
-    let obj: any = {}, i = 0, max = 0, count = 0;
+    let obj: { [key: string]: number } = {}, i = 0, max = 0, count = 0;
     for (i = 0; i < s.length; i++) {
-        if (s[i] in obj) {
-            count = 1;//1;
-            obj = {};//{[s[i]]:i};
-            i = obj[s[i]] + 1;
-        } else {
-            count++;
-            obj[s[i]] = i;
-        }
+        if (s[i] in obj) i = obj[s[i]], count = 0, obj = {};
+        else count++, obj[s[i]] = i;
         if (max < count) max = count;
     }
     return max;
 }
 
 console.log(lengthOfLongestSubstring("abcabcbb"));
-console.log(lengthOfLongestSubstring("bbbbb"));
-console.log(lengthOfLongestSubstring("pwwkew"));
-console.log(lengthOfLongestSubstring("aab"));
-console.log(lengthOfLongestSubstring("dvdf"));
