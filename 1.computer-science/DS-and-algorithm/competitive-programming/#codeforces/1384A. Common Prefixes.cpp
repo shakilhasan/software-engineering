dd5d8bd60bbcaa38b712d0fdea5cf06b512dcdda
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-12-03 12:28:13 +0600

    add 1384A. Common Prefixes.cpp

---
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#codeforces/1384A. Common Prefixes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+    int t,n,a,b;
+    char st[100]="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        cout<<""<<st<<endl;
+        while(n--)
+        {
+            cin>>a;
+            if (st[a]<'m')
+                st[a]=st[a]+7;
+            else
+                st[a]=st[a]-7;
+            cout<<st<<endl;
+        }
+
+    }
+}
