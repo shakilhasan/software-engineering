b9d27e717e71cef3de05048585ba62e2378c6246
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-12-04 01:06:25 +0600

    [Accepted] https://leetcode.com/problems/valid-anagram

--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/242.Valid Anagram.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/242.Valid Anagram.ts
@@ -1,26 +1,14 @@
 // https://leetcode.com/problems/valid-anagram
 function isAnagram(s: string, t: string): boolean {
-    let a = [...s];
-    let b=[...t];
+    let obj: { [k: string]: number } = {}
+    if (s.length != t.length) return false;
+    for (let i = 0; i < s.length; i++) {
+        (obj[s[i]]) ? obj[s[i]]++ : obj[s[i]] = 1;
+        (obj[t[i]]) ? obj[t[i]]-- : obj[t[i]] = -1;
+        if (obj[s[i]] === 0) delete obj[s[i]];
+        if (obj[t[i]] === 0) delete obj[t[i]];
+    }
+    return !Object.keys(obj).length;
+}
 
-    for(let i=0; i<a.length; i++){
-        const index = b.indexOf(a[i]);
-        if (index !== -1) {
-            b.splice(index, 1);
-        }
-    }
-    if(b.length || s.length!=t.length) return false
-    return true
-}
-function isAnagram2(s: string, t: string): boolean {
-    let hash:any={}
-    for(let i=0;i<s.length;i++){
-        if(hash[s[i]])hash[s[i]]=hash[s[i]]+1
-        else hash[s[i]]=1
-        if(hash[t[i]])hash[t[i]]=hash[t[i]]-1
-        else hash[t[i]]=-1
-    }
-    const valuesArray: number[] = Object.values(hash);
-    const sum: number = valuesArray.reduce((acc, curr) => acc + curr, 0);
-    return !sum;
-}
+console.log(isAnagram("ab", "ba"))
