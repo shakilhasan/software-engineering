476f971b9b9c06ca4392b8be709d40a18e14bd2a
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2024-02-22 15:26:57 +0600

    *WIP: 69.Sqrt(x)

--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/66.Plus One.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/66.Plus One.ts
@@ -10,8 +10,8 @@ export function plusOne(digits: number[]): number[] {
     return digits;
 }
 
-console.log(plusOne([1, 2, 3])) // [1, 2, 4]
-console.log(plusOne([4, 3, 2, 1])) // [4, 3, 2, 2]
-console.log(plusOne([0])) // [1]
-console.log(plusOne([9])) // [1, 0]
-console.log(plusOne([9,9])) // [1,0,0]
+console.log(plusOne([1, 2, 3])); // [1, 2, 4]
+console.log(plusOne([4, 3, 2, 1])); // [4, 3, 2, 2]
+console.log(plusOne([0])); // [1]
+console.log(plusOne([9])); // [1, 0]
+console.log(plusOne([9,9])); // [1,0,0]
