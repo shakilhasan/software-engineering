f6695edc1f9f41bc8d040895c463e2b1195cdfdf
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2024-02-25 16:17:42 +0600

    [Accepted] https://leetcode.com/problems/largest-perimeter-triangle

---
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/976.Largest Perimeter Triangle.ts
@@ -0,0 +1,7 @@
+function largestPerimeter(nums: number[]): number {
+    nums = nums.sort((a, b) => b - a);
+    for (let i = 0; i < nums.length - 2; i++) if (nums[i] < nums[i + 1] + nums[i + 2]) return nums[i] + nums[i + 1] + nums[i + 2]
+    return 0;
+}
+
+console.log(largestPerimeter([2, 1, 2])) //5
