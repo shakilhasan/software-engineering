88d6d5efda6e08226f3dbc6a00355fb6f036b408
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2024-02-25 13:00:54 +0600

    [Accepted] https://leetcode.com/problems/longest-palindrome

--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/409.Longest Palindrome.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/409.Longest Palindrome.ts
@@ -1,15 +1,16 @@
 //https://leetcode.com/problems/longest-palindrome
 function longestPalindrome(s: string): number {
-    const obj:any={};
-    let p=0,halfSum=0;
-    for(const char of s){
-        if(char in obj) obj[char]++;
-        else obj[char]=1;
+    const obj: any = {};
+    let p = 0, sum = 0;
+    for (const char of s) {
+        if (char in obj) obj[char]++;
+        else obj[char] = 1;
     }
-    for(const key in obj){
-        halfSum+=Math.floor(obj[key]/2)
-        if(!p) p=obj[key]%2;
+    for (const key in obj) {
+        sum += (obj[key] - obj[key] % 2)
+        if (!p) p = obj[key] % 2;
     }
-    return halfSum*2+p;
+    return sum + p;
 }
-console.log(longestPalindrome('bb'))
+
+console.log(longestPalindrome('bb')) //2
