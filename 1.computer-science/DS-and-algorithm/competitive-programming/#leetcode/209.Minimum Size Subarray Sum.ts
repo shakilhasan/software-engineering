9b3296f0244f941b5784f28b9f5ba6e1bbcb30ba
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2024-02-27 00:26:20 +0600

    [resubmit] https://leetcode.com/problems/minimum-size-subarray-sum

--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/209.Minimum Size Subarray Sum.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/209.Minimum Size Subarray Sum.ts
@@ -1,16 +1,16 @@
 function minSubArrayLen(target: number, nums: number[]): number {
     let min = 999999999, sum = 0, left = 0;
     for (let right = 0; right < nums.length; right++) {
-        sum = sum + nums[right];
-        while ( sum >= target) {
-            if (right - left < min) min = right - left;
-            sum = sum - nums[left++];
+        sum += nums[right];
+        while (sum >= target) {
+            if (right - left < min) min = right - left + 1;
+            sum -= nums[left++];
         }
     }
     if (min === 999999999) return 0;
-    return min + 1;
+    return min;
 }
 
 console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]))//2
-// console.log(minSubArrayLen(4, [1, 4, 4]))//1
-// console.log(minSubArrayLen(11, [1, 1, 1, 1, 1, 1, 1, 1]))//0
+console.log(minSubArrayLen(4, [1, 4, 4]))//1
+console.log(minSubArrayLen(11, [1, 1, 1, 1, 1, 1, 1, 1]))//0
