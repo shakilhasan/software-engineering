ed024f618466ee8290b2025e350d054b6cd7c25e
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2024-02-27 00:42:03 +0600

    [resubmit] https://leetcode.com/problems/binary-search

--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/704. Binary Search.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/704. Binary Search.ts
@@ -1,13 +1,8 @@
 //https://leetcode.com/problems/binary-search/
+import {binarySearch} from "./helpers/utils";
+
 function search(nums: number[], target: number): number {
-    let left = 0, right = nums.length - 1, mid = 0;
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        if (target > nums[mid]) left = mid + 1
-        else if (target < nums[mid]) right = mid - 1;
-        else return mid;
-    }
-    return -1
+    return binarySearch(nums, target)
 }
 
 console.log(search([-1, 0, 3, 5, 9, 12], 9))
--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/helpers/utils.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/helpers/utils.ts
@@ -2,6 +2,17 @@ function decimalToBinary(decimalNumber: number): number {
     return Number((decimalNumber >>> 0).toString(2));
 }
 
+function binarySearch(nums: number[], target: number): number {
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (target > nums[mid]) left = mid + 1
+        else if (target < nums[mid]) right = mid - 1;
+        else return mid;
+    }
+    return -1
+}
 export {
-    decimalToBinary
+    decimalToBinary,
+    binarySearch
 }
