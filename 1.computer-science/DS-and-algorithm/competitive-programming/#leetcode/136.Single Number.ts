2c67e2887b317d58c305ab1edb810ed69d667598
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2024-02-26 18:09:25 +0600

    [resubmit] https://leetcode.com/problems/single-number

--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/136.Single Number.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/136.Single Number.ts
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/single-number
 function singleNumber(nums: number[]): number {
     let result = 0;
     for (let i = 0; i < nums.length; i++) {
@@ -5,7 +6,17 @@ function singleNumber(nums: number[]): number {
     }
     return result;
 }
+
 function singleNumber2(nums: number[]): number {
-    return nums.reduce((c,s)=>c^s,0)
+    return nums.reduce((c, s) => c ^ s, 0)
 }
+
+//using object, hash
+function singleNumber3(nums: number[]): number {
+    const obj: any = {}
+    for (const num of nums) if (num in obj) obj[num] = 0; else obj[num] = 1;
+    for (const key in obj) if (obj[key]) return Number(key);
+    return 0;
+}
+
 
