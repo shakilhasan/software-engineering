71409fa3c83fb1a52a4492f788c59976a7bd13de
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2024-02-27 00:37:38 +0600

    [resubmit] https://leetcode.com/problems/binary-search

--- 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/704. Binary Search.ts
+++ 1.computer-science/DS-and-algorithm/competitive-programming/#leetcode/704. Binary Search.ts
@@ -1,13 +1,13 @@
+//https://leetcode.com/problems/binary-search/
 function search(nums: number[], target: number): number {
-    let start = 0;
-    let end = nums.length - 1;
-    while (start <= end) {
-        const mid = Math.floor((start + end) / 2)
-        if (target < nums[mid]) end = mid - 1;
-        else if (target > nums[mid]) start = mid + 1;
-        else if (target === nums[mid]) return mid;
+    let left = 0, right = nums.length - 1, mid = 0;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (target > nums[mid]) left = mid + 1
+        else if (target < nums[mid]) right = mid - 1;
+        else return mid;
     }
-    return -1;
+    return -1
 }
 
 console.log(search([-1, 0, 3, 5, 9, 12], 9))
