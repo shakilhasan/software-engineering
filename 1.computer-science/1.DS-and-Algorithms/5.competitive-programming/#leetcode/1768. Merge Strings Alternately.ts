22ca66154c41ad3feb1a663927fac9ab28d79fbe
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2024-10-03 20:02:31 +0600

    [Accepted]https://leetcode.com/problems/merge-strings-alternately

--- 1.computer-science/1.DS-and-Algorithms/5.competitive-programming/#leetcode/1768. Merge Strings Alternately.ts
+++ 1.computer-science/1.DS-and-Algorithms/5.competitive-programming/#leetcode/1768. Merge Strings Alternately.ts
@@ -1,10 +1,9 @@
 //https://leetcode.com/problems/merge-strings-alternately
 function mergeAlternately(word1: string, word2: string): string {
     const len1 = word1.length,len2 = word2.length;
-    let result='',p=0,q=0;
-    let i=0;
+    let result='', p=0, q=0, i=0;
     while (i < len1 +len2) {
-        if(i%2===0 && p<len1) result+=word1[p++];
+        if((i%2===0 && p<len1)||q>=len2) result+=word1[p++];
         else result+=word2[q++];
         i++;
     }
