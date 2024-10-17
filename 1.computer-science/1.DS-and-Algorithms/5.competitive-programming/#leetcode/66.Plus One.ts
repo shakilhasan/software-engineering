cb897a65214bf0c05b8ecfee148f8326e0f60f5f
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2024-10-17 19:32:48 +0600

    [Accepted] https://leetcode.com/problems/plus-one

--- 1.computer-science/1.DS-and-Algorithms/5.competitive-programming/#leetcode/66.Plus One.ts
+++ 1.computer-science/1.DS-and-Algorithms/5.competitive-programming/#leetcode/66.Plus One.ts
@@ -1,4 +1,5 @@
-export function plusOne(digits: number[]): number[] {
+//https://leetcode.com/problems/plus-one
+export function plusOne1(digits: number[]): number[] {
     for(let i = digits.length - 1; i >= 0; i--) {
         if(digits[i] < 9) {
             digits[i]++;
@@ -10,6 +11,14 @@ export function plusOne(digits: number[]): number[] {
     return digits;
 }
 
+export function plusOne(digits: number[]): number[] {
+    let c = 1, len = digits.length;
+    for(let i = len-1; i >= 0; i--)
+        if(digits[i] === 9 && c) digits[i] = 0;
+        else digits[i] += c,c=0;
+    if(c) digits.unshift(1);
+    return digits;
+}
 console.log(plusOne([1, 2, 3])); // [1, 2, 4]
 console.log(plusOne([4, 3, 2, 1])); // [4, 3, 2, 2]
 console.log(plusOne([0])); // [1]
