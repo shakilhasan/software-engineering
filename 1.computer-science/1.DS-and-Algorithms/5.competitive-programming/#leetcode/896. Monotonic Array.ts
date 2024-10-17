28cb224cf510fce6294302adc8dc90ede4a986d7
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2024-10-17 20:31:32 +0600

    [Accepted 2] https://leetcode.com/problems/monotonic-array

--- 1.computer-science/1.DS-and-Algorithms/5.competitive-programming/#leetcode/896. Monotonic Array.ts
+++ 1.computer-science/1.DS-and-Algorithms/5.competitive-programming/#leetcode/896. Monotonic Array.ts
@@ -1,11 +1,21 @@
 // https://leetcode.com/problems/monotonic-array
-function isMonotonic(nums: number[]): boolean {
+function isMonotonic1(nums: number[]): boolean {
     let p = 0;
     for (let i = 1; i < nums.length; i++) {
         if (!p) {
             if (nums[i - 1] < nums[i]) p = 1;
-            else if((nums[i - 1] > nums[i])) p = -1
+            else if ((nums[i - 1] > nums[i])) p = -1
         } else if ((p > 0 && nums[i - 1] > nums[i]) || (p < 0 && nums[i - 1] < nums[i])) return false;
+    }
+    return true;
+}
+
+function isMonotonic(nums: number[]): boolean {
+    let increasing = false, decreasing = false;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i - 1] < nums[i]) increasing = true;
+        else if ((nums[i - 1] > nums[i])) decreasing = true;
+        if (increasing && decreasing) return false;
     }
     return true;
 }
