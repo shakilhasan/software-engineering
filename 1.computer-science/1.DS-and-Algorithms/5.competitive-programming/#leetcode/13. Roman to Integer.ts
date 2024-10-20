f8de6cf2426d119b4a0d8e71b9655fa77ee57745
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2024-10-20 20:12:20 +0600

    [Accepted] https://leetcode.com/problems/roman-to-integer

--- 1.computer-science/1.DS-and-Algorithms/5.competitive-programming/#leetcode/13. Roman to Integer.ts
+++ 1.computer-science/1.DS-and-Algorithms/5.competitive-programming/#leetcode/13. Roman to Integer.ts
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/roman-to-integer
-function romanToInt(s: string): number {
+function romanToInt1(s: string): number {
     const obj: any = {
         I: 1,
         V: 5,
@@ -17,6 +17,24 @@ function romanToInt(s: string): number {
     return sum;
 }
 
+function romanToInt(s: string): number {
+    const obj: any = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    let result=0,c=0;
+    for(let i =s.length-1; i >=0; i--) {
+        if(c<=obj[s[i]])result+=obj[s[i]];
+        else result-=obj[s[i]]
+        c=obj[s[i]];
+    }
+    return result;
+};
 console.log(romanToInt("XIX"));
-console.log(romanToInt("MCMXCIV"));
+console.log(romanToInt("MCMXCIV"));//1994
 console.log(romanToInt("MCDLXXVI"));
