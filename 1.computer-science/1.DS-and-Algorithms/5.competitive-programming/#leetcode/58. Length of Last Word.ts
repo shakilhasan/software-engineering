2fe168b8d4121412f3b2247b443f17c0ac3b01ac
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2024-10-20 20:32:51 +0600

    [Accepted] https://leetcode.com/problems/length-of-last-word

--- 1.computer-science/1.DS-and-Algorithms/5.competitive-programming/#leetcode/58. Length of Last Word.ts
+++ 1.computer-science/1.DS-and-Algorithms/5.competitive-programming/#leetcode/58. Length of Last Word.ts
@@ -1,12 +1,13 @@
 // https://leetcode.com/problems/length-of-last-word
 function lengthOfLastWord(s: string): number {
-    let len = 0, i,p=0;
-    for (i = s.length-1; i >=0; i--) {
+    let len = 0, i, p = 0;
+    for (i = s.length - 1; i >= 0; i--) {
         if (s[i] != ' ') len++;
-        else if(len && s[i] == ' ') break;
+        else if (len && s[i] == ' ') break;
     }
     return len;
 }
-
 console.log(lengthOfLastWord("Hello World"))
 console.log(lengthOfLastWord("   fly me   to   the moon  "))
+console.log(lengthOfLastWord("a")) //1
+console.log(lengthOfLastWord("a ")) //1
